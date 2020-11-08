1b91d501209cb307c734c772ed8e4f7c284d97d0
Jonathan Fleckenstein <jonfleck88@gmail.com>
2020-11-08 12:47:00 -0500

    ready to add variables

--- src/convertFunctionCallToStringMacro.ts
+++ src/convertFunctionCallToStringMacro.ts
@@ -14,19 +14,37 @@ export default createMacro(({ references }) => {
       throw new MacroError("ct.macro can only be called as a function");
     }
 
-    const literals = callExpression.arguments.filter(function notEmpty<TValue>(
-      argument: TValue
-    ): argument is Extract<TValue, { value: any }> {
+    const literals = callExpression.arguments.filter(function nonLiterals<T>(
+      argument: T
+    ): argument is Extract<T, { value: any }> {
       return "value" in argument;
     });
 
+    type Argument = t.CallExpression["arguments"][number];
+
+    // const identifiers = callExpression.arguments.filter(function nonIdentifiers<
+    //   T extends Argument
+    // >(argument: T): argument is Extract<T, { type: "Identifier" }> {
+    //   return "type" in argument && argument.type === "Identifier";
+    // });
+
+    // If any args aren't literals or identifiers throw...
+
+    // let stringLiteral;
+
+    let argumentsNode;
+
     if (referencePath.parentPath.parentPath.type === "JSXExpressionContainer") {
-      referencePath.parentPath.parentPath.replaceWith(
-        t.stringLiteral(literals.join(" "))
-      );
+      argumentsNode = referencePath.parentPath.parentPath;
     } else {
-      referencePath.parentPath.replaceWith(t.stringLiteral(literals.join(" ")));
+      argumentsNode = referencePath.parentPath;
     }
+
+    argumentsNode.replaceWith(t.stringLiteral(literals.join(" ")));
+
+    // t.stringLiteral(literals.join(" ")
+
+    // stringLiteral[0].insertAfter(t.binaryExpression("+", t.));
   });
 
   return {
